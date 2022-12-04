ce83fdcd1190d0fdb00efe309163f030f506a91f
bataykindm <bataykin.dmitriy@mail.ru>
2022-12-04 18:51:41 +0700

    added SecurityConfig, AuthenticationController and its dto

---
+++ src/main/java/bdm.test/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package bdm.test.config;
+
+import bdm.test.security.jwt.JwtConfig;
+import bdm.test.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .apply(new JwtConfig(jwtTokenProvider));
+    }
+}
