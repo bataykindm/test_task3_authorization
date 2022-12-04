d1e0980de6efec13625f6bddb44391927aeaf021
bataykindm <bataykin.dmitriy@mail.ru>
2022-12-04 17:37:00 +0700

    created JwtAccount and JwtAccountDetailsService

---
+++ src/main/java/bdm.test/security/JwtAccountDetailsService.java
@@ -0,0 +1,31 @@
+package bdm.test.security;
+
+import bdm.test.entity.Account;
+import bdm.test.security.jwt.JwtAccount;
+import bdm.test.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtAccountDetailsService implements UserDetailsService {
+
+    private final AccountService accountService;
+
+    @Autowired
+    public JwtAccountDetailsService(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = accountService.getAccountByUsername(username);
+        JwtAccount jwtAccount = new JwtAccount(account);
+
+
+
+        return null;
+    }
+}
---
+++ src/main/java/bdm.test/security/jwt/JwtAccount.java
@@ -0,0 +1,74 @@
+package bdm.test.security.jwt;
+
+import bdm.test.entity.Account;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class JwtAccount implements UserDetails {
+
+    private final Long id;
+    private final String username;
+    private final String password;
+
+
+    @Autowired
+    public JwtAccount(Long id, String username, String password, boolean enabled, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public JwtAccount (Account account){
+        this.id = account.getId();
+        this.username = account.getUsername();
+        this.password = account.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @JsonIgnore // ????????????????????????????????
+    public Long getId() {
+        return id;
+    }
+
+    @JsonIgnore //?????????????????????????????????
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
